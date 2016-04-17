ba9669b6161f43353456b5469346ea114f8c7195
Yodde <szymon.klebowski@student.put.edu.pl>
2016-04-18 01:35:29 +0200

    Maps working
    Dodano przycisk otwierający mapy.
    Zaimplementowano OnClickListener w MainActivity by wygodniej obsługiwać
    przyciski.
    TODO:
    -dodawanie markerów z listy
    -centrowanie na pozycję telefonu/Poznań.

--- app/src/main/java/com/example/szymon/przewodnikturystyczny/MainActivity.java
+++ app/src/main/java/com/example/szymon/przewodnikturystyczny/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.szymon.przewodnikturystyczny;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,25 +17,39 @@ import android.widget.TextView;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     static List<String> urls = Arrays.asList("http://wti.mikroprint.pl/get_places.php?number=8", "http://wti.mikroprint.pl/get_place_details.php?Id=7");
     static Places places;
     static boolean downloadedPlacessComplete;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.main, new PlacesFragment())
                     .commit();
         }
         final Button downloadDataButton = (Button)findViewById(R.id.download_places_button);
-        if (downloadDataButton != null) {
-            downloadDataButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    new GetJson().execute(new GetJsonParameters(v.getContext(), urls));
-                }
-            });
+        downloadDataButton.setOnClickListener(this);
+        final Button showMapButton = (Button) findViewById(R.id.map_button);
+        showMapButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.download_places_button:{
+                new GetJson().execute(new GetJsonParameters(v.getContext(), urls));
+                break;
+            }
+            case R.id.map_button:{
+                final Intent myIntent = new Intent(MainActivity.this,CityMap.class);
+                startActivity(myIntent);
+                break;
+            }
+            default:
+                break;
         }
     }
 }
